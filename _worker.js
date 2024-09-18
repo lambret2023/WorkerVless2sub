bf01ca90ed02695040d0b10d633ec762b485bb20
lambret2023 <135798479+lambret2023@users.noreply.github.com>
2024-09-18 14:37:37 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,8 @@ let mytoken= ['auto'];//快速订阅访问入口, 留空则不启动快速订阅
 // 设置优选地址，不带端口号默认443，TLS订阅生成
 let addresses = [
 	'icook.tw:2053#官方优选域名',
-	'cloudflare.cfgo.cc#优选官方线路',
+	'cf.090227.xyz:443#cf090227',
+	'liangge.us.kg'
 ];
 
 // 设置优选地址api接口
