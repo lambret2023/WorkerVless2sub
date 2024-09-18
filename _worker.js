884901f820456582fc8a17bbc66039fee01e382c
lambret2023 <135798479+lambret2023@users.noreply.github.com>
2024-09-19 07:25:04 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -8,14 +8,14 @@ let addresses = [
 	
 	'cf.090227.xyz:443#cf090227',
 	'liangge.us.kg',
-	'visa.com',
+
         'ct.090227.xyz',
 	'cmcc.090227.xyz',
 ];
 
 // 设置优选地址api接口
 let addressesapi = [
-	//"https://addressesapi.090227.xyz/cmcc",
+	"https://addressesapi.090227.xyz/cmcc",
 	//"https://addressesapi.090227.xyz/CloudFlareYes",
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
